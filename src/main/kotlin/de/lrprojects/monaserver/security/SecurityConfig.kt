f08f4ecefd85e82bfeea789bd96e1a01ceefab28
Lukas Reim <lukasr101@gmail.com>
2024-02-05 16:04:51 +0100

    security filter not working

--- src/main/kotlin/de/lrprojects/monaserver/security/SecurityConfig.kt
+++ src/main/kotlin/de/lrprojects/monaserver/security/SecurityConfig.kt
@@ -1,37 +1,38 @@
 package de.lrprojects.monaserver.security
 
-import de.lrprojects.monaserver.helper.TokenHelper
 import de.lrprojects.monaserver.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.PropertySource
-import org.springframework.core.annotation.Order
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.web.filter.OncePerRequestFilter
 
 
 @EnableWebSecurity
 @Configuration
 @EnableMethodSecurity
-class WebSecurityConfig (@Autowired val userRepository: UserRepository, @Autowired val jwtFilter: JWTFilter) {
+class WebSecurityConfig (@Autowired val jwtFilter: JWTFilter, @Autowired  val userDetailsService: MyUserDetailsService) {
 
 
     @Bean
-    fun userDetailsService(): OncePerRequestFilter? {
-        return JWTFilter(userRepository, TokenHelper())
+    fun ignoreResources(): WebSecurityCustomizer? {
+        return WebSecurityCustomizer { webSecurity: WebSecurity ->
+            webSecurity
+                .ignoring()
+                .requestMatchers("/public/**", "/signup", "/login", "/", "/recover")
+        }
     }
 
     @Bean
@@ -39,13 +40,7 @@ class WebSecurityConfig (@Autowired val userRepository: UserRepository, @Autowir
         http
             .csrf{ it.disable() } // Disabling CSRF for stateless session management
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)} // Stateless session management
-            .authorizeHttpRequests { authz ->
-                authz.requestMatchers( "/public/**").permitAll() // Permitting public endpoints
-                    .requestMatchers("/api/**").authenticated()
-            }
-
-            .authenticationProvider(authenticationProvider())
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .authorizeHttpRequests { it.requestMatchers("/api/**").authenticated() }
 
         return http.build();
     }
@@ -59,7 +54,7 @@ class WebSecurityConfig (@Autowired val userRepository: UserRepository, @Autowir
     @Bean
     fun authenticationProvider(): AuthenticationProvider? {
         val authenticationProvider = DaoAuthenticationProvider()
-        authenticationProvider.setUserDetailsService(userDetailsService())
+        authenticationProvider.setUserDetailsService(userDetailsService)
         authenticationProvider.setPasswordEncoder(passwordEncoder())
         return authenticationProvider
     }
