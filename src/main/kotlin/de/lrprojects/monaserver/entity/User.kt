377dbf5fb7c9952e4ab315bd87d939859f640dea
Lukas Reim <lukasr101@proton.me>
2026-03-21 13:01:54 +0100

    Fix expiration type to be mutable (#292)

--- src/main/kotlin/de/lrprojects/monaserver/entity/User.kt
+++ src/main/kotlin/de/lrprojects/monaserver/entity/User.kt
@@ -50,7 +50,6 @@ data class User (
     var resetPasswordUrl: String? = null,
 
     @Column
-    @CreationTimestamp
     var resetPasswordExpiration: OffsetDateTime? = null,
 
     @Column(nullable = false)
@@ -70,7 +69,6 @@ data class User (
     var code: String? = null,
 
     @Column
-    @Basic(fetch = FetchType.LAZY)
     var codeExpiration: OffsetDateTime? = null,
 
     @Column
